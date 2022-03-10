17035b7626ce0b8bba3fac959dd4108fa52fcfb9
dawit yenew <wizdevaxo.06464@gmail.com>
2022-03-11 02:02:27 +0300

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
+
 /**
-   * main - prints to the standard error
-   *
-   * Return: one
-   */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
