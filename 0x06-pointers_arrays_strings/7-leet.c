5d12baf6859512cada975f7fbbdcc6b200c4fde2
dawit yenew <wizdevaxo.06464@gmail.com>
2022-03-23 22:02:31 +0300

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,29 +1,33 @@
 #include "main.h"
-
 /**
- * leet - encode
- * @s: pointer to char params
- * Return: *s
+ * leet -  encodes a string into 1337.
+ * @s: string to code
+ * Return: the enconde string.
  */
-
 char *leet(char *s)
 {
 	int i;
 	int j;
-	char ch1[] = "aeotl";
-	char ch2[] = "AEOTL";
-	char leetChar[] = "43071";
-	
-	for (i = 0; s[i] != '\0'; i++)
+	int k;
+
+	char l[] = "oOlLeEaAtT";
+	char e[] = "0011334477";
+
+	i = 0;
+	while (s[i] != '\0')
 	{
-		for (j = 0; j < 5; j++)
+		j = 0;
+		k = 0;
+		while (l[j] != '\0')
 		{
-			if (s[i] == ch1[j] || s[i] == ch2[j])
+			if (s[i] == l[j])
 			{
-				s[i] = leetChar[j];
+				k = j;
+				s[i] = e[k];
 			}
+			j++;
 		}
+		i++;
 	}
 	return (s);
 }
-
