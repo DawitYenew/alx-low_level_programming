494f6f3146b6c340019dc2490b6bc217b3cd2c6c
dawit yenew <wizdevaxo.06464@gmail.com>
2022-04-14 23:08:28 +0300

    Variadic functions
    qqqq

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,28 +1,30 @@
-#include "variadic_functions.h"
-#include <stdarg.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 /**
- * print_numbers - prints all numbers with a separator
- * @separator: separe numbers
- * @n: int, number of undefined arguments
+ * print_strings - prints strings, followed by a new line
+ * @separator: delimiter
+ * @n: n args
+ * Return: void
  */
-
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
-va_list list;
-unsigned int i;
+	va_list arguments;
+	unsigned int s;
+	char *clone;
 
-va_start(list, n);
+	va_start(arguments, n);
 
-for (i = 0; i < n; i++)
-{
-	printf("%i", va_arg(list, int));
-	if (i != n - 1 && separator != NULL)
-		printf("%s", separator);
-}
-va_end(list);
-
-putchar('\n');
+	for (s = 0; s < n; s++)
+	{
+		clone = va_arg(arguments, char*);
+		if (clone != NULL)
+			printf("%s", clone);
+		else
+			printf("%p", clone);
+		if (separator != NULL && s < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(arguments);
 }
